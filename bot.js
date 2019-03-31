28888f842f730083b8bb227a2ad2e3d7f31669b3
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-03-31 15:00:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2806,22 +2806,19 @@ client.on('message', message => {
     }
 }
 });
-
-
-
 client.on("message", (message) => {
-    /// ALPHA CODES
-   if (message.content.startsWith("=ticket")) {     /// ALPHA CODES
-        const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
-        if (!message.guild.roles.exists("name", "BP)) return message.channel.send(`This server doesn't have a \`Ticket \` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+ 
+   if (message.content.startsWith("=new")) {  
+        const reason = message.content.split(" ").slice(1).join(" ");  
+        if (!message.guild.roles.exists("name", "Helpers Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Helpers Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "BP");
+            let role = message.guild.roles.find("name", "Helpers Team");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
-            });    /// ALPHA CODES
+            });  
             c.overwritePermissions(role2, {
                 SEND_MESSAGES: false,
                 READ_MESSAGES: false
@@ -2830,10 +2827,10 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+            message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
+                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, `)
                 .setTimestamp();
             c.send({
                 embed: embed
@@ -2845,18 +2842,18 @@ client.on("message", (message) => {
   if (message.content.startsWith("=close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-       message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
-           .then((m) => {
-               message.channel.awaitMessages(response => response.content === '#confirm', {
+       message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب =yes`)
+          .then((m) => {
+               message.channel.awaitMessages(response => response.content === '=yes', {
                        max: 1,
                        time: 10000,
                        errors: ['time'],
-                   })    /// ALPHA CODES
+                   })  
                    .then((collected) => {
                        message.channel.delete();
-                   })    /// ALPHA CODES
+                   })  
                    .catch(() => {
-                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                       m.edit('لقد إنتهت مدة التأكيد, التذكرة لم تنقفل').then(m2 => {
                            m2.delete();
                        }, 3000);
                    });
@@ -2864,6 +2861,9 @@ client.on("message", (message) => {
    }
  
 });
+
+
+
 
 client.on('guildMemberAdd', member => {
 let channel = member.guild.channels.find(c => c.name === 'chat');
